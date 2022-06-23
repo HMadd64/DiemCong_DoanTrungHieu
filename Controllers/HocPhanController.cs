c88b0077a09eccc54ec33a9fcb2814eb82fd9aae
Trung Hiếu <106734395+HMadd64@users.noreply.github.com>
2022-06-23 15:16:45 +0700

    diemcong

---
+++ Controllers/HocPhanController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DiemCong_DoanTrungHieu.Models;
+
+namespace DiemCong_DoanTrungHieu.Controllers
+{
+    public class HocPhanController : Controller
+    {
+        DataClassesDataContext db = new DataClassesDataContext();
+        public ActionResult Index()
+        {
+            var hp = from s in db.HocPhans select s;
+            return View(hp);
+        }
+        public ActionResult Details(string id)
+        {
+            var hp = db.HocPhans.Where(m => m.MaHP == id).First();
+            return View(hp);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(FormCollection collection, HocPhan s)
+        {
+            var mahp = collection["MaHP"];
+            var tenhp = collection["TenHP"];
+            var tinchi = Convert.ToInt32(collection["SoTinChi"]);
+            if (string.IsNullOrEmpty(tenhp))
+            {
+                ViewData["Error"] = "Don't empty!";
+            }
+            else
+            {
+                s.MaHP = mahp;
+                s.TenHP = tenhp.ToString();
+                s.SoTinChi = tinchi;
+                db.HocPhans.InsertOnSubmit(s);
+                db.SubmitChanges();
+                return RedirectToAction("Index");
+            }
+            return this.Create();
+        }
+    }
+}
---
+++ Controllers/SinhVienController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DiemCong_DoanTrungHieu.Models;
+
+namespace DiemCong_DoanTrungHieu.Controllers
+{
+    public class SinhVienController : Controller
+    {
+        DataClassesDataContext db = new DataClassesDataContext();
+        public ActionResult Index()
+        {
+            var sv = from s in db.SinhViens select s;
+            return View(sv);
+        }
+        public ActionResult Details(string id)
+        {
+            var sv = db.SinhViens.Where(m => m.MaSV == id).First();
+            return View(sv);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(FormCollection collection, SinhVien s)
+        {
+            var masv = collection["MaSV"];
+            var tensv = collection["HoTen"];
+            var gioitinh = collection["GioiTinh"];
+            var ngaysinh = Convert.ToDateTime(collection["NgaySinh"]);
+            var E_hinh = collection["Hinh"];
+            if (string.IsNullOrEmpty(tensv))
+            {
+                ViewData["Error"] = "Don't empty!";
+            }
+            else
+            {
+                s.MaSV = masv;
+                s.HoTen = tensv.ToString();
+                s.Hinh = E_hinh.ToString();
+                s.GioiTinh = gioitinh;
+                s.NgaySinh = ngaysinh;
+                db.SinhViens.InsertOnSubmit(s);
+                db.SubmitChanges();
+                return RedirectToAction("Index");
+            }
+            return this.Create();
+        }
+        public string ProcessUpload(HttpPostedFileBase file)
+        {
+            if (file == null)
+            {
+                return "";
+            }
+
+            file.SaveAs(Server.MapPath("~/Content/images/" + file.FileName));
+
+            return "/Content/images/" + file.FileName;
+        }
+
+    }
+}
